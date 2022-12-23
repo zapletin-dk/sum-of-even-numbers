d6042ce9620a0198c26b54836bbaaab8fb34d55a
Zapletin Daniil <Zaplot1994@gmail.com>
2022-12-23 14:45:06 +0300

    Task completed with all tests passed

--- src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
@@ -1,17 +1,16 @@
 package com.epam.rd.autotasks.arrays;
 
 public class SumOfEvenNumbers {
-
-    public static void main(String[] args) {
-        int[] array = new int[]{1, 3, 2, 8, 15, 199};
-
-        System.out.println(sum(array));
-    }
-
     public static int sum(int[] array){
-
-        //put your code here
-
-        throw new UnsupportedOperationException();
+        int sum =0;
+        if (array == null || array.length == 0){
+            return sum;
+        }
+        for (int var: array) {
+            if (var%2 == 0){
+                sum += var;
+            }
+        }
+        return sum;
     }
 }
